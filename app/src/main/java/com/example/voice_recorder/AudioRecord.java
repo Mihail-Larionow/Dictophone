d981fb158b60caa0e6b382196309352f924f2b4f
michel <mihail.larionow2001@gmail.com>
2023-03-04 04:28:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/voice_recorder/AudioRecord.java
@@ -0,0 +1,26 @@
+package com.example.voice_recorder;
+
+public class AudioRecord {
+
+    private int time;
+    private String name;
+
+    public AudioRecord(){
+
+    }
+
+    //Listening audio
+    public void listen(){
+
+    }
+
+    //Recording audio
+    public void record(){
+
+    }
+
+    //Deleting audio
+    public void delete(){
+
+    }
+}
